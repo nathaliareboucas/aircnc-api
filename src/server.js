96d7d3e49e440d57ca3d2906f6a197b4edf01631
Nathalia <nathreboucas5@gmail.com>
2019-11-01 23:58:36 -0300

    criando o arquivo de rotas, rota teste e conectando no banco com mongoose

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
+mongoose.connect('mongodb+srv://dbaaircnc:dbaaircnc@dbaircnc-lauk1.mongodb.net/aircnc?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({message: 'Hello AirCnC'});
-});
+app.use(routes);
 
 app.listen(3333);
