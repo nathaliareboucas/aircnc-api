f12128b73746fccff098a6be53f904077b0fd1cc
Nathalia <nathreboucas5@gmail.com>
2019-11-03 22:16:55 -0300

    recuperando thumbnail do spot

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://dbaaircnc:dbaaircnc@dbaircnc-lauk1.mongodb.net/a
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
